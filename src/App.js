4cd28ff2749fde16d43386117219c6b4e2bcbf70
Egor <101923873+monolit322@users.noreply.github.com>
2023-09-07 20:45:01 +0300

    last commit

--- src/App.js
+++ src/App.js
@@ -1,15 +1,63 @@
-import React from 'react';
-import './index.scss';
-import { Success } from './components/Success';
-import { Users } from './components/Users';
+import React from "react";
+import "./index.scss";
+import { Success } from "./components/Success";
+import { Users } from "./components/Users";
+import { useState } from "react";
+import { useEffect } from "react";
 
 // Тут список пользователей: https://reqres.in/api/users
 
 function App() {
+  const [users, setUsers] = useState([]);
+  const [invites, setInvites] = useState([]);
+  const [isLoading, setLoading] = useState(true);
+  const [searchValue, setSearchValue] = useState('');
+  const [success, setSuccess] = useState(false);
+
+  useEffect(() => {
+    fetch("https://reqres.in/api/users")
+      .then((res) => res.json())
+      .then((json) => {
+        setUsers(json.data);
+      })
+      .catch((err) => {
+        console.warn(err);
+        alert("Ошибка при получении пользователей");
+      })
+      .finally(() => setLoading(false));
+  }, []);
+
+  const onChangeSeachValue = (event) => {
+    setSearchValue(event.target.value);
+  };
+
+  const onClickInvite = (id) => {
+    if (invites.includes(id)) {
+      setInvites((prev) => prev.filter((_id) => _id !== id));
+    } else {
+      setInvites((prev) => [...prev, id]);
+    }
+  };
+
+  const onClickSendInvites = () => {
+    setSuccess(true);
+  }
+
   return (
     <div className="App">
-      <Users />
-      {/* <Success /> */}
+      {success ? (
+        <Success count={invites.length}/>
+      ) : (
+        <Users
+          onChangeSeachValue={onChangeSeachValue}
+          searchValue={searchValue}
+          items={users}
+          isLoading={isLoading}
+          invites={invites}
+          onClickInvite={onClickInvite}
+          onClickSendInvites={onClickSendInvites}
+        />
+      )}
     </div>
   );
 }
